f711720a64485494d8de9569b4109a04f2693961
godori <gej.okpo@gmail.com>
2018-08-08 08:33:38 +0900

    card click and move with animation

--- dwcard-script.js
+++ dwcard-script.js
@@ -11,6 +11,9 @@ addCardInfo(5, 1, 'bronze', '여우', '파이어폭스');
 $(document).ready(function () {
   console.log('dw card script init');
 
+  $('.card').click(function(){
+    game.moveCard(this);
+  });
 
   $('.card .point').hide();
   $('.card .desc').hide();
@@ -106,6 +109,29 @@ var game = {
 
       game.selectFaction()
     })
+
+  },
+  moveCard: (target) => {
+    console.log(target);
+    var card = $(target);
+    var playLine = $('.player-board .card-play');
+    
+    var oldOffset = card.offset();
+    card.appendTo(playLine);
+    var newOffset = card.offset(); 
+    
+    var temp = card.clone().appendTo('body');
+    temp.css({
+      'position': 'absolute',
+      'left': oldOffset.left,
+      'top': oldOffset.top,
+      'z-index': 1000
+    });
+    card.hide();
+    temp.animate({'top': newOffset.top, 'left': newOffset.left}, 'slow', function(){
+      card.show();
+      temp.remove();
+   });
 
   },
   selectFaction: () => {
